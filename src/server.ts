16f8da4d136719c35ce1c90d5957d3ec9e304276
Willian Silva <silva.williansantos@gmail.com>
2021-05-30 21:46:37 -0300

    Rollback

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ import './database';
 const port: number = config.get('App.port');
 
 app.listen(port, () => {
-  logger.info(`🏃 Running Server on port ${port}`);
+  logger.info(`🏃🚀 Running Server on port ${port}`);
 });
